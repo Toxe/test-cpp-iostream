9c491d93a196c926c67f3752aa1ca8a11a887778
Toxe <toxe@toxe.net>
2019-06-15 18:01:23 +0200

    streambuf_uppercase_writer.cpp

---
+++ streambuf_uppercase_writer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <streambuf>
+
+class streambuf_uppercase_writer : public std::streambuf {
+public:
+    streambuf_uppercase_writer(std::ostream& sink) : sink_(sink) {};
+
+private:
+    int_type overflow(int_type ch) override;
+
+private:
+    std::ostream& sink_;
+};
+
+
+streambuf_uppercase_writer::int_type streambuf_uppercase_writer::overflow(int_type ch)
+{
+    if (ch != traits_type::eof()) {
+        // convert lowercase to uppercase and write the character to output
+        ch = std::toupper(static_cast<char>(ch), getloc());
+        sink_.put(ch);
+
+        if (sink_.eof())
+            return traits_type::eof();
+    }
+
+    return traits_type::to_int_type(ch);
+}
+
+
+int main()
+{
+    streambuf_uppercase_writer uwsb(std::cout);
+    std::ostream out(&uwsb);
+
+    out << "dies ist ein test. ein kurzer satz. noch einer." << std::endl;
+}
